9eca0174a74cbd9cc7ef44add62958b65aa02488
Ellaiza <ellaizadajang22@gmail.com>
2025-02-13 21:32:32 -0800

    Added a Java Application

---
+++ Dajang_IT13A/src/Prelim/Act1_Arithmetic_Operators.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Prelim;
+
+/**
+ *
+ * @author pc3
+ */
+public class Act1_Arithmetic_Operators {
+    
+    public static void main(String[] args){
+        int fnum = 125;
+        int snum = 24;
+        
+        int add = fnum + snum;
+        int sub = fnum - snum;
+        int mul = fnum * snum;
+        int div = fnum / snum;
+        int mod = fnum % snum;
+        
+        System.out.println("This is the output of arithmetic Operators");
+        System.out.println("125 + 24 = " + add);
+        System.out.println("125 - 24 = " + sub);
+        System.out.println("125 x 24 = " + mul);
+        System.out.println("125 mod 24 = " + mod);
+        
+    }
+    
+}
